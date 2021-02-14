664fbf32aeda86c40d6fa2c850743c55589a233d
adilminhas2222 <74412445+adilminhas2222@users.noreply.github.com>
2021-02-14 21:00:48 +0000

    Create events.h

---
+++ events.h
@@ -0,0 +1,28 @@
+#ifndef STAT1_H
+#define STAT1_H
+#ifndef STAT2_H
+#define STAT2_H
+#ifndef RESOURCE1_H
+#define RESOURCE1_H
+#ifndef RESOURCE2_H
+#define RESOURCE2_H
+#ifndef COMBAT1_H
+#define COMBAT1_H
+#ifndef COMBAT2_H
+#define COMBAT2_H
+#include <iostream>
+using namespace std;
+
+int stat1();
+int stat2();
+int resource1();
+int resource2();
+int combat1();
+int combat2();
+
+#endif
+#endif
+#endif
+#endif
+#endif
+#endif
