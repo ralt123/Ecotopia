7e4552f427cd039a11b26a1951c6dc8244580076
adilminhas2222 <74412445+adilminhas2222@users.noreply.github.com>
2021-02-14 20:59:06 +0000

    Create randomevents.h

---
+++ randomevents.h
@@ -0,0 +1,9 @@
+#ifndef RANDOMCHANCE_H //-----------------------------randomevents.h-----------------------------------
+#define RANDOMCHANCE_H
+#include <iostream>
+using namespace std;
+
+int randomchance(time_t current_time, bool movement);
+
+#endif
+#endif
