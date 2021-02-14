f62fb2845e44fb982afd96b48d699cdd763300ea
adilminhas2222 <74412445+adilminhas2222@users.noreply.github.com>
2021-02-14 21:02:03 +0000

    Create randomdata.h

---
+++ randomdata.h
@@ -0,0 +1,17 @@
+#ifndef LASTEVENT_H
+#define LASTEVENT_H
+#ifndef LASTEVENTADD_H
+#define LASTEVENTADD_H
+#ifndef LASTEVENTRESET_H
+#define LASTEVENTRESET_H
+#include <iostream>
+using namespace std;
+
+
+int lastevent();
+int lasteventadd();
+int lasteventreset();
+
+#endif
+#endif
+#endif
