d091e956982f23b0f500a3ac90d87a5bb0b8f287
adilminhas2222 <74412445+adilminhas2222@users.noreply.github.com>
2021-02-14 21:00:24 +0000

    Create events.cpp

---
+++ events.cpp
@@ -0,0 +1,37 @@
+#include <iostream>//-------------------------------------this is a placeholder until we I know how to implement the random events------------------
+using namespace std;
+
+int stat1(){
+    cout << "you have found a suspicous mushroom growing " << endl;
+    cout << "you eat the mushroom (you have increased strength) " << endl;
+    cout << "status increase" << endl;
+    cout << "do drugs kids" << endl;
+    return 0;
+}
+int stat2(){
+    cout << "you fell into stinging nettles" << endl;
+    cout << "you have recieved damage" << endl;
+    cout << "status decrease" << endl;
+    return 0;
+}
+int resource1(){
+    cout << "you have encountered a lonely orphan" << endl;
+    cout << "you have kidnapped a lonely orphan" << endl;
+    cout << "resource increase" << endl;
+    return 0;
+}
+int resource2(){
+    cout << "fucking monkey steals some food" << endl;
+    cout << "resource decrease" << endl;
+    return 0;
+}
+int combat1(){
+    cout << "hostile mob appeared" << endl;
+    cout << "fight the hostile mob" << endl;
+    return 0;
+}
+int combat2(){
+    cout << "very raw beef appeared" << endl;
+    cout << "fight the cow" << endl;
+    return 0;
+}
