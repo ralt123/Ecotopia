cbc810bf3d893cfe5ba6646c96491175b8b62fa2
gavriel384 <72576929+gavriel384@users.noreply.github.com>
2021-02-15 20:30:45 +0200

    Inventory
    Evangelos Gavriel

---
+++ Inventory.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <ostream>
+#include <Windows.h>
+#include <string>
+#include <cctype>
+using namespace std;
+
+  struct Item {
+    string name; //Item name.
+    int slot; //Head, Torso, Hands
+    int hp;
+    int attack;
+    int defense;
+    int oxygen;
+    
+  };
+
+std::ostream &operator<<(std::ostream &os, const Item& item) {
+    os << item.name;
+    return os;
+}
+
+int main()
+{
+
+    //Variables, Strings, etc.
+    int itemcounter = 0, counter = 0;
+
+
+    //"Empty" Item
+    Item Empty{ "<Empty>", 0, 0, 0 };
+
+    vector<Item> Equipment = { 10, Empty }; //Current Equipment, 10 empty slots.
+    vector<Item> Inventory = { }; //Players Inventory.
+    string InventorySlots[] = { "Head" "Torso", "Hands" }; //Player parts where items can be equiped.
+
+    cout << "You check your manned maneuvering unit," << " you have:" << endl;
+
+    for (int i = 0; i < itemcounter; i++)
+    {
+        cout << InventorySlots[i];
+        if (Equipment[i].name == "Empty ")
+        {
+        cout << " " << Equipment[i] << endl << endl;
+      }
+    }
+
+}
