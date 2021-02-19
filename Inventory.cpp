98a92c4cdb359f92e654bf6e7c9dd065f93643d7
gavriel384 <72576929+gavriel384@users.noreply.github.com>
2021-02-19 18:16:17 +0200

    Update Inventory.cpp

--- Inventory.cpp
+++ Inventory.cpp
@@ -33,7 +33,7 @@ int main()
     //"Empty" Item
     Item Empty{ "<Empty>", 0, 0, 0 };
 
-    vector<Item> Equipment = { 10, Empty }; //Current Equipment, 10 empty slots.
+    vector<Item> Equipment = { 100, Empty }; //Current Equipment, 10 empty slots.
     vector<Item> Inventory = { }; //Players Inventory.
     string InventorySlots[] = { "Head" "Torso", "Hands" }; //Player parts where items can be equiped.
 
