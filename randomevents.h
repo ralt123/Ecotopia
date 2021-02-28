b2d60c29d9a11bc3e99733fc2e1687b2a9535a0d
adilminhas2222 <74412445+adilminhas2222@users.noreply.github.com>
2021-02-28 17:26:49 +0000

    Update randomevents.h

--- randomevents.h
+++ randomevents.h
@@ -1,9 +1,9 @@
 #ifndef RANDOMCHANCE_H //-----------------------------randomevents.h-----------------------------------
 #define RANDOMCHANCE_H
 #include <iostream>
+#include "characters.h"
 using namespace std;
 
-int randomchance(time_t current_time, bool movement);
+int randomchance(time_t current_time, bool movement, Character player);
 
-#endif
 #endif
