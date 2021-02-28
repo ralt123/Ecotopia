1321ed8c655d2e8a1c6e2edf98df4cdfbfec5cce
adilminhas2222 <74412445+adilminhas2222@users.noreply.github.com>
2021-02-28 17:25:34 +0000

    Update events.cpp
    added some of the eventd

--- events.cpp
+++ events.cpp
@@ -1,37 +1,42 @@
 #include <iostream>//-------------------------------------this is a placeholder until we I know how to implement the random events------------------
+#include "characters.h"
 using namespace std;
 
-int stat1(){
+int stat1(Character player){
     cout << "you have found a suspicous mushroom growing " << endl;
-    cout << "you eat the mushroom (you have increased strength) " << endl;
-    cout << "status increase" << endl;
+    cout << "you eat the mushroom (you have increased strength)(doing health for now) " << endl;
+    player.heal(2);
+    cout << player.get_health() << endl;
     cout << "do drugs kids" << endl;
     return 0;
 }
-int stat2(){
+int stat2(Character player){
     cout << "you fell into stinging nettles" << endl;
     cout << "you have recieved damage" << endl;
-    cout << "status decrease" << endl;
+    player.directReduceHealth(1);
+    cout << player.get_health() << endl;
     return 0;
 }
-int resource1(){
+int resource1(Character player){
     cout << "you have encountered a lonely orphan" << endl;
     cout << "you have kidnapped a lonely orphan" << endl;
     cout << "resource increase" << endl;
     return 0;
 }
-int resource2(){
+int resource2(Character player){
     cout << "fucking monkey steals some food" << endl;
     cout << "resource decrease" << endl;
     return 0;
 }
-int combat1(){
+int combat1(Character player){
     cout << "hostile mob appeared" << endl;
     cout << "fight the hostile mob" << endl;
+    engageCombat(Player &User, Alien &Foe);
     return 0;
 }
-int combat2(){
+int combat2(Character player){
     cout << "very raw beef appeared" << endl;
     cout << "fight the cow" << endl;
+    engageCombat(Player &User, Alien &Foe);
     return 0;
 }
