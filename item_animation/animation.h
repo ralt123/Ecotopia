122d9024a476b5f29fe3a405710bbb5e6c294b89
Jupiter <peter.mellen11@gmail.com>
2021-04-15 14:47:50 +0100

    4

--- item_animation/animation.h
+++ item_animation/animation.h
@@ -7,23 +7,60 @@ class Animation {
     public:
         std::ifstream frame;
 
-        void output(std::string frame_num) {
+        void output1() {
+            std::ifstream frame2;
             std::string lines;
-            frame.open(frame_num);
-            if (frame.is_open()) {
-                while (std::getline(frame, lines)) {
+            frame2.open("C:/Users/peter/Documents/GitHub/Ecotopia/item_animation/frame1.txt");
+            if (frame2.is_open()) {
+                while (std::getline(frame2, lines)) {
                 std::cout << lines << "\n";
                 }
-                
             }
-            else {
-                std::cout << "Not Found";
+        }
+
+        void output2() {
+            std::ifstream frame2;
+            std::string lines;
+            frame2.open("C:/Users/peter/Documents/GitHub/Ecotopia/item_animation/frame2.txt");
+            if (frame2.is_open()) {
+                while (std::getline(frame2, lines)) {
+                std::cout << lines << "\n";
+                }
+            }
+        }
+
+        void output3() {
+            std::ifstream frame2;
+            std::string lines;
+            frame2.open("C:/Users/peter/Documents/GitHub/Ecotopia/item_animation/frame3.txt");
+            if (frame2.is_open()) {
+                while (std::getline(frame2, lines)) {
+                std::cout << lines << "\n";
+                }
             }
         }
 
         void wait_timer() {
             std::chrono::milliseconds time(500);
             std::this_thread::sleep_for(time);
+
+            
+        }
+
+        void full_animation() {
+            output1();
+            wait_timer();
+            system("CLS");
+            output2();
+            wait_timer();
+            system("CLS");
+            output3();
+            wait_timer();
+            system("CLS");
+
         }
 
 };
+
+
+
